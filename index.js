2f1a871b6030730f4f319fe02577a7197a7e0b5b
Ryeker Herndon <ryekerjh@protonmail.com>
2021-03-30 18:39:49 -0600

    docs(cipher): add steps as function head docs

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+// We check that the charCode for the provided letter falls within the uppercase range
+const isUpperCase = (letter) => letter.charCodeAt() >= 65 && letter.charCodeAt() <= 90;
+
+/** 
+ *  1. Take a word to translate (string) and a shift value (number)
+ *  2. Check for validity of both values
+ *  3. Declare a return string (empty)
+ *  4. Loop over the word, checking each letter for case in order to shift correctly according to the charCode values
+ *  5. Return the shifted string
+*/
 const cipher = (word, shift) => {
     if (!word || typeof word !== "string") throw new Error("You must provide a valid string to shift");
     if (!shift || isNaN(parseInt(shift))) throw new Error("The cipher shift amount must be a valid number");
